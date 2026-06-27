393c3ce81e2fbc1c112b57eae49fa61355f7d276
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-27 17:39:59 -0300

    feat: add asset paths, colors, menu options and window dimensions

--- code/Consts.py
+++ code/Consts.py
@@ -1,3 +1,23 @@
+# P
+BG_MENU_PATH = "assets/background/sky_background/background 1/orig.png"
+BG_GAME_PATH = "assets/background/sky_background/background 3/orig.png"
+MENU_MUSIC_PATH = "assets/music/menu_music.mp3"
+GAME_MUSIC_PATH = "assets/music/game_music.mp3"
+
+# C
+C_BLACK = (0, 0, 0)
+C_GREEN = (0, 140, 0)
+C_PURPLE = (128, 0, 128)
+C_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = [
+    'PLAY',
+    'SCORE',
+    'EXIT'
+]
+
 # W
-WIN_HEIGHT = 600
-WIN_WIDTH = 800
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
+FPS = 60
