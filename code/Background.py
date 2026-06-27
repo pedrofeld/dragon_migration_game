480083bed840407c26947879b75e450d70354651
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-27 17:46:57 -0300

    feat: create background class

--- code/Background.py
+++ code/Background.py
@@ -1,12 +1,18 @@
 import pygame
 
+from code.Consts import WIN_WIDTH, WIN_HEIGHT, BG_MENU_PATH
+
 class Background():
-    def __init__(self):
-        self.image = pygame.image.load("../img/background.png")
+    def __init__(self, window, path_image=BG_MENU_PATH):
+        self.window = window
+        self.path_image = path_image
+        self.image = pygame.image.load(str(path_image)).convert()
+        self.image = pygame.transform.scale(self.image, (WIN_WIDTH, WIN_HEIGHT))
         self.scroll_speed = 1
 
     def update(self):
         pass
 
     def draw(self):
-        pass
+        if self.window is not None:
+            self.window.blit(self.image, (0, 0))
