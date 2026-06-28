a26aff2e966979d281b5f463215f5929c781cbc0
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-28 10:32:32 -0300

    feat: add assets paths (dragon and bird images)

--- code/Consts.py
+++ code/Consts.py
@@ -1,10 +1,8 @@
-# P
-BG_MENU_PATH = "assets/background/sky_background/background 3/orig.png"
+# B
 BG_GAME_PATH = "assets/background/sky_background/background 1/orig.png"
+BG_MENU_PATH = "assets/background/sky_background/background 3/orig.png"
 BG_SCORE_PATH = "assets/background/floor_background/Backgrounds/backgroundCastles.png"
-MENU_MUSIC_PATH = "assets/music/menu_music.mp3"
-GAME_MUSIC_PATH = "assets/music/game_music.mp3"
-SCORE_MUSIC_PATH = "assets/music/score_music.mp3"
+BIRD_IMAGE_PATH = "assets/personagens/birds/Bird 1/Bird Classic 1.png"
 
 # C
 C_BLACK = (0, 0, 0)
@@ -12,14 +10,26 @@ C_GREEN = (0, 140, 0)
 C_PURPLE = (128, 0, 128)
 C_WHITE = (255, 255, 255)
 
+# D
+DRAGON_IMAGE_PATH = "assets/personagens/dragons/Basic Dragon Animations/Viridian Drake/ViridianDrake.gif"
+
+# F
+FPS = 60
+
+# G
+GAME_MUSIC_PATH = "assets/music/game_music.mp3"
+
 # M
+MENU_MUSIC_PATH = "assets/music/menu_music.mp3"
 MENU_OPTION = [
     'PLAY',
     'SCORE',
     'EXIT'
 ]
 
+# S
+SCORE_MUSIC_PATH = "assets/music/score_music.mp3"
+
 # W
-WIN_WIDTH = 576
 WIN_HEIGHT = 324
-FPS = 60
+WIN_WIDTH = 576
